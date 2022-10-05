845a40183766d297b947210c88004f4b3ac352aa
Sophearah <sophearah.suy-puth@mail.mcgill.ca>
2022-10-05 00:38:08 -0400

    creating components and using props

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { TodoItem } from './TodoItem';
 
 // Create a functional component
 export default function App() {
 	return (
 		<div>
-			<p> my app </p>
+			<TodoItem taskName="COMP 251 A1"/>
+			<TodoItem taskName="COMP 303 A1"/>
+			<TodoItem taskName="finish making bootcamp materials"/>
 		</div>
 	);
 }
