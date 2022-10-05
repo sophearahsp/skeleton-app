515cab1a9b3a379ca5bcb7f9a78a508310ac8d56
Sophearah <sophearah.suy-puth@mail.mcgill.ca>
2022-10-05 00:56:16 -0400

    created button and event handling function to add to todoCount in state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { TodoItem } from './TodoItem';
+import { Button } from '@mui/material';
 
 // Create a functional component
 export default function App() {
+    const [todoCount, setTodoCount] = useState(0);
+
+    const onClickButton = () => {
+        setTodoCount(todoCount + 1);
+        console.log("Button clicked");
+    };
+
 	return (
 		<div>
-			<TodoItem taskName="COMP 251 A1"/>
-			<TodoItem taskName="COMP 303 A1"/>
-			<TodoItem taskName="finish making bootcamp materials"/>
+            <Button onClick={onClickButton} variant="contained">
+                basic mui button
+            </Button>
+            {todoCount}
 		</div>
 	);
 }
