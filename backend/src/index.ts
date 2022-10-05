f549101f9cedbeeddf3b072c3a20dc0c21aee67f
SymPiracha <sym.piracha@gmail.com>
2022-10-05 16:04:11 -0400

    added a few comments

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,17 +5,19 @@ import * as cors from "cors";
 
 // Import from other .ts files
 import { AppDataSource } from "./data-source";
+import { resolve } from "path";
 
 AppDataSource.initialize().then(async () => {
     // Create express app
     const app = express();
 
     // Using third party middleware
-    app.use(bodyParser.json());
-    app.use(cors());
+    app.use(bodyParser.json()); // body-parser is which allows express to read the body and then parse that into a Json object that we can understand.
+    app.use(cors()); //
 
     // Routes go here
-    
+    // Creating route : https://expressjs.com/en/guide/routing.html
+
     // Starts server and listens on port 3001 for connections
     app.listen(3001);
 })
